5344b6232fee6aa8213c3a54ea5aee2a34385fc0
Piotr Persona <persona.piotr@gmail.com>
2019-08-22 21:14:52 +0200

    Refactor ./build.sh code
    Signed-off-by: Piotr Persona <persona.piotr@gmail.com>

--- build.sh
+++ build.sh
@@ -1,27 +1,61 @@
 #!/usr/bin/env bash
 
-PACKAGE="${PACKAGE:-"main.go"}"
-PACKAGE_NAME="${PACKAGE_NAME:-"docku"}"
+function __help() {
+    echo "build docku binary"
+    echo
+    echo "Supported platforms: ${PLATFORMS[@]}"
+    echo
+    echo "Usage"
+    echo
+    echo "./build.sh"
+    echo
+    echo "Options:"
+    echo
+    echo "  -h --help    Display help message"
+}
 
-PLATFORMS=(
-    "darwin/386"
-    "darwin/amd64"
-    "linux/386"
-    "linux/amd64"
-)
+function __config() {
+    PACKAGE="${PACKAGE:-"main.go"}"
+    PACKAGE_NAME="${PACKAGE_NAME:-"docku"}"
 
-for platform in "${PLATFORMS[@]}"; do
-    PLATFORM_SPLIT=( ${platform//\// } )
-    GOOS=${PLATFORM_SPLIT[0]}
-    GOARCH="${PLATFORM_SPLIT[1]}"
-    OUTPUT_NAME="${PACKAGE_NAME}-${GOOS}-${GOARCH}"
-    if [[ "${GOOS}" = "windows" ]]; then
-        OUTPUT_NAME+='.exe'
-    fi
+    PLATFORMS=(
+        "darwin/386"
+        "darwin/amd64"
+        "linux/386"
+        "linux/amd64"
+    )
+}
 
-    env GOOS="${GOOS}" GOARCH="${GOARCH}" go build -o "${GOPATH}/bin/${OUTPUT_NAME}" "${PACKAGE}"
-    if [[ "${?}" -ne 0 ]]; then
-        echo 'An error has occurred! Aborting the script execution...'
-        exit 1
-    fi
-done
+function __handle_args() {
+    while [[ "${#}" -gt 0 ]]; do
+        case "${1}" in
+            -h|--help) __help; exit 0;;
+        esac
+    done
+}
+
+function __build_docku() {
+    for platform in "${PLATFORMS[@]}"; do
+        local PLATFORM_SPLIT=( ${platform//\// } )
+        local GOOS=${PLATFORM_SPLIT[0]}
+        local GOARCH="${PLATFORM_SPLIT[1]}"
+        local OUTPUT_NAME="${PACKAGE_NAME}-${GOOS}-${GOARCH}"
+        if [[ "${GOOS}" = "windows" ]]; then
+            OUTPUT_NAME+='.exe'
+        fi
+
+        env GOOS="${GOOS}" GOARCH="${GOARCH}" go build -o "${GOPATH}/bin/${OUTPUT_NAME}" "${PACKAGE}"
+        if [[ "${?}" -ne 0 ]]; then
+            echo 'An error has occurred! Aborting the script execution...'
+            exit 1
+        fi
+    done
+}
+
+function main() {
+    __config
+    __handle_args "${@}"
+    __build_docku
+}
+
+main "${@}"
