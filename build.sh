884448e7a7aa2786ff113be3a1039a12a2de9fd1
Piotr Persona <persona.piotr@gmail.com>
2019-08-22 23:19:57 +0200

    Add versioning
    Fix: #8
    
    Signed-off-by: Piotr Persona <persona.piotr@gmail.com>

--- build.sh
+++ build.sh
@@ -24,6 +24,16 @@ function __config() {
         "linux/386"
         "linux/amd64"
     )
+
+    GIT_TAG="$( git describe --abbrev=0 )"
+    GIT_HASH="$( git rev-parse HEAD )"
+    BUILD_DATE="$( date +%F )"
+    DOCKU_REPO="github.com/piotrpersona/docku"
+    read -r -d '' LDFLAGS << EOM
+        -X ${DOCKU_REPO}/cmd.gitVersionTag=${GIT_TAG}
+        -X ${DOCKU_REPO}/cmd.gitHash=${GIT_HASH}
+        -X ${DOCKU_REPO}/cmd.buildDate=${BUILD_DATE}
+EOM
 }
 
 function __handle_args() {
@@ -40,11 +50,14 @@ function __build_docku() {
         local GOOS=${PLATFORM_SPLIT[0]}
         local GOARCH="${PLATFORM_SPLIT[1]}"
         local OUTPUT_NAME="${PACKAGE_NAME}-${GOOS}-${GOARCH}"
+
         if [[ "${GOOS}" = "windows" ]]; then
             OUTPUT_NAME+='.exe'
         fi
 
-        env GOOS="${GOOS}" GOARCH="${GOARCH}" go build -o "${GOPATH}/bin/${OUTPUT_NAME}" "${PACKAGE}"
+        env GOOS="${GOOS}" GOARCH="${GOARCH}" go build \
+            -ldflags "${LDFLAGS}" \
+            -o "${GOPATH}/bin/${OUTPUT_NAME}" "${PACKAGE}"
         if [[ "${?}" -ne 0 ]]; then
             echo 'An error has occurred! Aborting the script execution...'
             exit 1
